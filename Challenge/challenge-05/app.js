9c45755a93563abd5e4ab3f62d8914fe78aaa1dd
Michael Peter <michael_piterct@hotmail.com>
2021-04-28 00:28:56 -0300

    challenge05

---
+++ Challenge/challenge-05/app.js
@@ -0,0 +1,16 @@
+new Vue({
+	el: '#challenge05',
+	data: {
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		book: {
+			title: 'O Senhor dos Anéis',
+			author: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			name: 'Maria',
+			grades: [7.67, 8.33, 6.98, 9.21]
+		}
+	}
+});
