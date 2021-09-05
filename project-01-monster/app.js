f6e11f73c87a8368bcdda617820f9e4e7e1a15b2
Michael Peter <michael_piterct@hotmail.com>
2021-09-05 00:31:34 -0300

    Panel Result

--- project-01-monster/app.js
+++ project-01-monster/app.js
@@ -2,10 +2,12 @@ new Vue({
     el: '#gameApp',
     data: {
         playerLife: 100,
-        monsterlife: 100,
+        monsterlife: 0,
     },
     computed: {
-
+        hasResult() {
+            return this.playerLife == 0 || this.monsterlife == 0
+        }
     },
     methods: {
 
