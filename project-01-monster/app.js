7ba06f42aa60deed4fd4a8a2a83f27038e76e7aa
Michael Peter <michael_piterct@hotmail.com>
2021-09-05 01:13:40 -0300

    Attack button

--- project-01-monster/app.js
+++ project-01-monster/app.js
@@ -15,6 +15,18 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterlife = 100
+        },
+        attack(especial) {
+            this.hurt(7,12, false);
+        },
+        hurt(min, max, especial) {
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this.playerLife = Math.max(this.playerLife - hurt, 0)
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value);
         }
     },
     watch: {
