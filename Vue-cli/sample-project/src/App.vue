da95755a7b19ad763e34f55b9a290748aca53975
Michael Peter <michael_piterct@hotmail.com>
2021-09-25 18:08:56 -0300

    teste using vue js

--- Vue-cli/sample-project/src/App.vue
+++ Vue-cli/sample-project/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ title }}
+    <button @click="title += '#'">Alter</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      title: "Test using Vue Js !!",
+    };
+  },
+};
 </script>
 
-<style>
+<style >
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: chocolate;
+  color: #fff;
 }
 </style>
