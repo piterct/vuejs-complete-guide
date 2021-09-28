c709db538f8f1a263b5e6902cbe0e1e2b7038c94
Michael Peter <michael_piterct@hotmail.com>
2021-09-27 23:57:49 -0300

    Footer component

--- Components/challenge-component/src/App.vue
+++ Components/challenge-component/src/App.vue
@@ -4,9 +4,7 @@
       <v-toolbar flat>
         <v-list>
           <v-list-tile>
-            <v-list-tile-title class="title">
-                Menu
-            </v-list-tile-title>
+            <v-list-tile-title class="title"> Menu </v-list-tile-title>
           </v-list-tile>
         </v-list>
       </v-toolbar>
@@ -26,43 +24,42 @@
     </v-navigation-drawer>
 
     <v-app-bar app>
-        <v-toolbar-title class="headline text-uppercase">
-				<span>Desafio</span>
-				<span class="font-weight-light">Componente</span>
-        </v-toolbar-title>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Desafio</span>
+        <span class="font-weight-light">Componente</span>
+      </v-toolbar-title>
     </v-app-bar>
 
     <v-content>
       <v-carousel>
         <v-carousel-item
-            v-for="(item,i) in fotos"
-            :key="i"
-            :src="item.src"></v-carousel-item>
+          v-for="(item, i) in fotos"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
       </v-carousel>
     </v-content>
-
-    <v-footer class="pa-3" app>
-      <v-spacer></v-spacer>
-      <div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
-    </v-footer>
+    <app-footer/>
   </v-app>
 </template>
 
 <script>
+import Footer from "./Footer.vue";
 export default {
+  components: { "app-footer": Footer },
   data() {
     return {
       itensMenu: [
-        {titulo: 'Início', icon: 'mdi-view-dashboard'},
-        {titulo: 'Sobre', icon: 'mdi-forum'}
+        { titulo: "Início", icon: "dashboard" },
+        { titulo: "Sobre", icon: "question_answer" },
       ],
       fotos: [
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
-      ]
-    }
-  }
-}
+        { src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" },
+        { src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg" },
+        { src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg" },
+        { src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg" },
+      ],
+    };
+  },
+};
 </script>
---
+++ Components/challenge-component/src/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-footer class="pa-3" app>
+     <v-spacer></v-spacer>
+     <div>Vue &copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style >
+</style>
+
