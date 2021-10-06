6d73f99e3f8f1bd0ea806568facfb54c082adbf6
Michael Peter <michael_piterct@hotmail.com>
2021-10-06 01:12:03 -0300

    information of user

---
+++ Components/comunication-component/src/components/InfoUser.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="componente">
+    <h2>The information of user</h2>
+    <p>Many details...</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
+</style>
--- Components/comunication-component/src/components/User.vue
+++ Components/comunication-component/src/components/User.vue
@@ -1,40 +1,40 @@
 <template>
-    <div class="container">
-        <h1>User component</h1>
-        <p>This component is pretty cool !!!</p>
-        <hr>
-        <div class="components">
-            <app-info-user />
-            <app-edit-user />
-        </div>
+  <div class="container">
+    <h1>User component</h1>
+    <p>This component is pretty cool !!!</p>
+    <hr />
+    <div class="components">
+      <app-info-user />
+      <app-edit-user />
     </div>
+  </div>
 </template>
 
 <script>
-import AppInfoUser from './UsuarioInfo'
-import AppEditUser from './EditUser'
+import AppInfoUser from "./InfoUser";
+import AppEditUser from "./EditUser";
 
 export default {
-    components: { AppInfoUser, AppEditUser }
-}
+  components: { AppInfoUser, AppEditUser },
+};
 </script>
 
 <style scoped>
-    .container {
-        background-color: #27363b;
-        color: #fff;
-        padding: 10px;
-    }
+.container {
+  background-color: #27363b;
+  color: #fff;
+  padding: 10px;
+}
 
-    .container hr {
-        margin: 20px 10px;
-    }
+.container hr {
+  margin: 20px 10px;
+}
 
-    .components {
-        display: flex;
-    }
+.components {
+  display: flex;
+}
 
-    .components > * {
-        margin: 10px;
-    }
+.components > * {
+  margin: 10px;
+}
 </style>
