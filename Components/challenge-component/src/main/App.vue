fced9e181a7a20693669c1d5f39028f5d9201cc4
Michael Peter <michael_piterct@hotmail.com>
2021-10-05 21:40:42 -0300

    main foulder

--- Components/challenge-component/src/main/App.vue
+++ Components/challenge-component/src/main/App.vue
@@ -10,10 +10,10 @@
 </template>
 
 <script>
-import Menu from "./components/template/Menu.vue";
-import Toolbar from "./components/template/Toolbar.vue";
-import Carousel from "./components/widgets/Carousel.vue";
-import Footer from "./components/template/Footer.vue";
+import Menu from "../components/template/Menu.vue";
+import Toolbar from "../components/template/Toolbar.vue";
+import Carousel from "../components/widgets/Carousel.vue";
+import Footer from "../components/template/Footer.vue";
 
 export default {
   components: {
