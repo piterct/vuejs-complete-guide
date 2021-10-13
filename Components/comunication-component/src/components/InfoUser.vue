d28c9cfef436a4598e1bd22698bb6394f41be5ae
Michael Peter <michael_piterct@hotmail.com>
2021-10-13 05:54:38 -0300

    default prop using funtion

--- Components/comunication-component/src/components/InfoUser.vue
+++ Components/comunication-component/src/components/InfoUser.vue
@@ -15,6 +15,9 @@ export default {
       type: String,
       // required: true,
       default: "anonymous",
+      // default: function () {
+      // return Array(10).fill(0).join(",");
+      // },
     },
   },
   methods: {
