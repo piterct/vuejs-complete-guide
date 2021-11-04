98918181f823fa033801e4f4f177911cad7af585
Michael Peter <michael_piterct@hotmail.com>
2021-11-03 23:37:24 -0300

    life cycle of component

--- Components/advanced-components/quotes/src/components/Quote.vue
+++ Components/advanced-components/quotes/src/components/Quote.vue
@@ -14,6 +14,12 @@ export default {
   destroyed() {
     console.log("destroyed");
   },
+  activated() {
+    console.log("activated");
+  },
+  deactivated() {
+    console.log("deactivated");
+  }
 };
 </script>
 
